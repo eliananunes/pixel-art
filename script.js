ef8e11179dd2687d50f9e7814eb45bc918938312
eliananunes <elianadnunes@gmail.com>
2022-07-27 19:38:06 -0300

    refatorada basica no script

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-function selectColor(event) {
+const colorPalette = document.querySelector('#color-palette');
+const pixel = document.getElementsByClassName('pixel');
+const button = document.getElementById('clear-board');
+
+function selectColor(e) {
     const selectedColor = document.querySelector('.selected');
     selectedColor.classList.remove('selected');
-    event.target.classList.add('selected');
+    e.target.classList.add('selected');
 }
 
-const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', selectColor);
 
 
 function paintPixel(e) {
     const selectedColor = document.querySelector('.selected').id;
     e.target.style.backgroundColor = selectedColor;
-  }
-  
-  const pixel = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
+}
+
+for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].addEventListener('click', paintPixel);
 }
 
 function clearBoard() {
-    const pixel = document.getElementsByClassName('pixel');
     for (let index = 0; index < pixel.length; index += 1) {
         pixel[index].style.backgroundColor= 'white';
     }
 }
 
-const button = document.getElementById('clear-board');
 button.addEventListener('click', clearBoard);
